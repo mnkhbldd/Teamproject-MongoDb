74a08cfbdd70f1d3e499c20967dcf82656e7eb60
mnkhbldd <mnkhbldd@gmail.com>
2025-06-19 05:17:07 +0800

    review data edit

--- Back-end/controllers/review.ts
+++ Back-end/controllers/review.ts
@@ -82,7 +82,7 @@ export const getReviewsByUserCompanies = async (
 
     const reviews = await ReviewModel.find({ company: { $in: companyIds } })
       .populate("company", "name")
-      .populate("user", "name")
+      .populate("user", "username", "photo")
       .sort({ createdAt: -1 });
 
     res.status(200).json({
